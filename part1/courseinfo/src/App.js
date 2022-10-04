4340ef23bd33d263ebce9a220f56e97a2f118c70
Alejandro Segovia <fcblade676@gmail.com>
2022-10-03 21:03:22 -0500

    Version 1.3 - Modificando las variables en el componente App

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,9 +9,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exercise={props.exercise1}/>
-      <Part part={props.part2} exercise={props.exercise2}/>
-      <Part part={props.part3} exercise={props.exercise3}/>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
     </>
   );
 };
@@ -38,26 +38,32 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <>
       <Header title={course} />
       <Content
-        part1={part1}
-        exercise1={exercises1}
-        part2={part2}
-        exercise2={exercises2}
-        part3={part3}
-        exercise3={exercises3}
+        part1={part1.name}
+        exercise1={part1.exercises}
+        part2={part2.name}
+        exercise2={part2.exercises}
+        part3={part3.name}
+        exercise3={part3.exercises}
       />
 
-      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+      <Total ex1={part1.exercises} ex2={part2.exercises} ex3={part3.exercises} />
     </>
   );
 };
